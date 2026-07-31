f119b23688587bdf76a68ecddfa70a185f8ed233
Simarpreet Singh <simarsaini13@gmail.com>
2026-07-31 22:24:37 +0100

    Finished navbar

---
+++ script.js
@@ -0,0 +1,10 @@
+const hamburger = document.querySelector(".hamburger")
+const links = document.querySelector(".nav-links")
+
+hamburger.addEventListener("click", () => {
+    links.classList.toggle("active")
+})
+
+links.addEventListener("click", () => {
+    links.classList.toggle("active")
+})
